467a87193529981570522daa0483a3206961398c
Syaiful <syaifulscooter@gmail.com>
2021-12-22 19:31:23 +0700

    membuat simpan checkout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\User\CheckoutController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 use App\Http\Controllers\UserController;
@@ -24,14 +25,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 
-Route::get('checkout', function () {
-    return view('checkout');
-})->name('checkout');
+Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
 
 
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
+// Route::get('success-checkout', function () {
+// })->name('success-checkout');
 
 // Socialite route
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
